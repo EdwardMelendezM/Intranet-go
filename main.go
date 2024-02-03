feb121f8fa39fbb44bcb4365ac9ac6c10a079330
EdwardMelendezM <edward.melendez.mendigure@gmail.com>
2024-02-03 13:41:45 -0500

    firts commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+
+	"auth/entities"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.POST("api/v1/login", loginHandler)
+
+	err := router.Run(":8080")
+	if err != nil {
+		return
+	}
+}
+
+func loginHandler(context *gin.Context) {
+	var loginBody entities.LoginBody
+
+	if err := context.ShouldBindJSON(&loginBody); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+	if loginBody.Username == "usuario" && loginBody.Password == "contrasena" {
+		context.JSON(http.StatusOK, gin.H{"message": "Login exitoso"})
+	} else {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Credenciales incorrectas"})
+	}
+}
