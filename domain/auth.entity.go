e3908148eeb1ed2e643eb4d51bedca77915ffbae
EdwardMelendezM <edward.melendez.mendigure@gmail.com>
2024-02-04 22:45:11 -0500

    adding changes

--- domain/auth.entity.go
+++ domain/auth.entity.go
@@ -6,19 +6,19 @@ type LoginBody struct {
 }
 
 type LoginResponse struct {
-	Status       int
-	Error        *string
-	RefreshToken *string
+	Status       int     `json:"status" binding:"required"`
+	Error        *string `json:"error"`
+	RefreshToken *string `json:"refresh_token"`
 }
 
 type RegisterBody struct {
-	Username string
-	Password string
-	Birthday string
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+	Birthday string `json:"birthday" binding:"required"`
 }
 
 type RegisterResponse struct {
-	Status       int
-	Error        *string
-	RefreshToken *string
+	Status       int     `json:"status" binding:"required"`
+	Error        *string `json:"error"`
+	RefreshToken *string `json:"refresh_token"`
 }
--- domain/auth.repository.ts.go
+++ domain/auth.repository.ts.go
@@ -2,5 +2,6 @@ package domain
 
 type AuthRepository interface {
 	Login(body LoginBody) LoginResponse
+	VerifyExistingUser(body LoginBody) (bool bool, err error)
 	Register(body RegisterBody) RegisterResponse
 }
